a0b664e371189c3e6c2506cfe1af1ffbf1790e1b
craig.thompson <craig.thompson@communicatorcorp.com>
2016-12-05 16:17:36 +0000

    Fix createRequestHandler call

--- modules/index.js
+++ modules/index.js
@@ -283,7 +283,7 @@ export const createServer = (options) => {
   if (options.https) {
     return https.createServer(
       options.https,
-      createRequestHandler
+      createRequestHandler(options)
     )
   }
 
